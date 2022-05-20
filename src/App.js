c21600414c0e21980decc3b482c2376fde82ae08
Kurt Guardia <kurtguardia@gmail.com>
2022-05-20 19:46:21 +0200

    matching cards

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { src: '/img/helmet-1.png' },
-  { src: '/img/potion-1.png' },
-  { src: '/img/ring-1.png' },
-  { src: '/img/scroll-1.png' },
-  { src: '/img/shield-1.png' },
-  { src: '/img/sword-1.png' },
+  { src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false },
+  { src: '/img/ring-1.png', matched: false },
+  { src: '/img/scroll-1.png', matched: false },
+  { src: '/img/shield-1.png', matched: false },
+  { src: '/img/sword-1.png', matched: false },
 ]
 
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
 
   // shuffle cards for new game
   const shuffleCards = () => {
@@ -25,7 +47,17 @@ function App() {
     setTurns(0)
   }
 
-  console.log(cards, turns)
+  // handle a choice
+  const handleChoice = (card) => {
+    !choiceOne ? setChoiceOne(card) : setChoiceTwo(card)
+  }
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns((prevTurns) => prevTurns + 1)
+  }
 
   return (
     <div className='App'>
@@ -34,7 +66,16 @@ function App() {
 
       <div className='card-grid'>
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={
+              card === choiceOne ||
+              card === choiceTwo ||
+              card.matched
+            }
+          />
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import './SingleCard.css'
 
-function SingleCard({ card }) {
+function SingleCard({ card, handleChoice, flipped }) {
+  const handleClick = () => {
+    handleChoice(card)
+  }
+
   return (
     <div className='card'>
-      <img
-        src={card.src}
-        alt='card front'
-        className='front'
-      />
-      <img
-        src='/img/cover.png'
-        alt='card back'
-        className='back'
-      />
+      <div className={flipped ? 'flipped' : ''}>
+        <img
+          src={card.src}
+          alt='card front'
+          className='front'
+        />
+        <img
+          src='/img/cover.png'
+          alt='card back'
+          className='back'
+          onClick={handleClick}
+        />
+      </div>
     </div>
   )
 }
